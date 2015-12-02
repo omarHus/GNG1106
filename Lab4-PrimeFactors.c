833529c82a3d047ce8c188b829e82f3d2f4ca3cf
Omar Husain <omar.husain2@gmail.com>
2015-12-01 20:10:40 -0500

    Simplification of earlier code

--- Lab4-PrimeFactors.c
+++ Lab4-PrimeFactors.c
@@ -4,13 +4,9 @@
 
 #define PRIME_MAX 1000000
 
-// function prototypes
-int isPrime(int user_input);
-void print_prime_factors(int user_input);
-
 int main(void)
 {
-    int user_input, is_prime;
+    int user_input, i, isPrime = 1;
     printf("Enter an integer between 2 and %d\n", PRIME_MAX);
     scanf("%d", &user_input);
 
@@ -19,64 +15,27 @@ int main(void)
         printf("You did not enter an integer in the required range.\n");
     else
     {
-        // determine whether number is prime
-        is_prime = isPrime(user_input);
-
-        if(is_prime)
+        for(i=1;i<user_input/2;i++)
+        {
+            while (user_input!= 0)
+            {
+                if(user_input%i==0)
+                {
+                    printf("%d", i);
+                    user_input=user_input/2;
+                    isPrime++;
+                }
+            }
+        }
+        if(is_prime==1)
         {
             printf("The number %d is prime!\n", user_input);
         }
         else
         {
             printf("The number %d is not prime!\n", user_input);
-            printf("%d = ", user_input);
-            // call the function to print prime factors of our input when not prime
-            print_prime_factors(user_input);
         }
     }
     return 0;
 }
 
-int isPrime(int user_input)
-{
-    int i, is_prime = 1;
-
-    for(i=2;i<user_input; i++)
-    {
-        // if any number factors into our input then it is not a prime
-        if(user_input%i == 0)
-        {
-            // not a prime
-            is_prime = 0;
-            break;
-        }
-    }
-
-    return is_prime;
-}
-
-void print_prime_factors(int user_input)
-{
-    int divisor, number, counter;
-    number = user_input;
-    divisor = 2;
-    counter = 0;
-
-    while(number > 1)
-    {
-        // check if divisor is a prime factor
-        if(number%divisor == 0)
-        {
-            // checking counter in order to place multiplication sign ("x") in the correct spot
-            if(counter == 0)
-                printf("%d", divisor);
-            else
-                printf("x%d", divisor);
-            // divide our number by the divisor to generate the next factor
-            number = number/divisor;
-            counter++;
-        }
-        else
-            divisor++;
-    }
-}
